9f0eaedafd4f3f1d8b4188c93f446183d79552f0
t <t2358545272@gmail.com>
2024-04-07 15:59:33 +0800

    consider x == 10

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -50,7 +50,7 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x;
